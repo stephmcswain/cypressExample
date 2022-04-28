818504ccb3eaeb5dffaacad96df815d77a66cc5b
Steph McSwain <stephanie@mailshake.com>
2022-04-28 11:11:15 -0400

    updates

---
+++ cypress/integration/test.js
@@ -0,0 +1,8 @@
+describe('test some random stuff', () => {
+    it('equals true', () => {
+        expect(true).to.equal(true)
+    })
+    it('equals false', () => {
+        expect(false).to.equal(false)
+    })
+})
---
+++ cypress/integration/test_2.js
@@ -0,0 +1,8 @@
+describe('test some random stuff', () => {
+    it('goes to google', () => {
+        cy.visit('www.google.com')
+    })
+    it('goes to wral', () => {
+        cy.visit('www.wral.com')
+    })
+})
