e416f213227a0e771487cfaaa40d4263c3c31f02
Steph McSwain <stephanie@mailshake.com>
2022-04-28 11:47:45 -0400

    fixes

--- cypress/integration/test_2.js
+++ cypress/integration/test_2.js
@@ -2,7 +2,4 @@ describe('test some random stuff', () => {
     it('goes to google', () => {
         cy.visit('www.google.com')
     })
-    xit('goes to wral', () => {
-        cy.visit('www.wral.com')
-    })
 })
