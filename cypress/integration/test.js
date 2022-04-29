e460a45bef5e6ff53503a5924d6fb6c05520a9aa
Steph McSwain <stephanie@mailshake.com>
2022-04-29 13:03:15 -0400

    test

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,6 +1,7 @@
 describe('test some random stuff', () => {
     it('equals true', () => {
         expect(true).to.equal(false)
+        expect(true).to.equal(true)
     })
     it('equals false', () => {
         expect(false).to.equal(false)
